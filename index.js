533ce15b995a2a04e15c3422ef0bac7a28ac1076
Karen Nashipai <karen.karei@student.moringaschool.com>
2022-09-26 11:21:38 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+const receivesAFunction = (callbackFunction)   => {
+    return callbackFunction();
+}
+const returnsANamedFunction = () => {
+    function Afunction(){
+
+    }
+    return Afunction
+}
+const returnsAnAnonymousFunction = () =>{
+    return () =>{
+        
+    }
+    
+    
+
+} 
+
+
+
+    
+
+
+
